e268f41fa1858b016b5b157408ca703ea1657d06
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-06-08 17:03:55 -0400

    made the to string method cleaner

--- interface.py
+++ interface.py
@@ -8,5 +8,4 @@ class Interface:
         self.bssid = bssid
 
     def tostring(self):
-        return 'name: ' + str(self.name) + ', essid: ' + str(self.essid) + ', mode: ' + str(self.mode) + ', bssid: ' + str(self.bssid)
-        #return 'name: %s, essid: %s, mode: %s, bssid: %s' % str(self.name), str(self.essid), str(self.mode), str(self.bssid)
+        return 'name: %s, essid: %s, mode: %s, bssid: %s' % (str(self.name), str(self.essid), str(self.mode), str(self.bssid))
