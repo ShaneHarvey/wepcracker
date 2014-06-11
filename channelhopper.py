5669255e2330e9ffeeff1fde8c732e0f922c01f1
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-06-11 18:29:02 -0400

    crated a thread that makes the NIC hop channels

---
+++ channelhopper.py
@@ -0,0 +1,28 @@
+import os
+import threading
+import time
+
+__author__ = 'Michael'
+
+
+class ChannelHopper(threading.Thread):
+    running = False
+    iface = None
+
+    def __init__(self, iface):
+        threading.Thread.__init__(self)
+        self.iface = iface
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        channels = range(1, 14)  # outside for loop so we do not create list every time
+        self.running = True
+
+        while self.running:
+            for channel in channels:
+                if not self.running:
+                    break
+                os.system('iwconfig %s channel %d' % (self.iface, channel))
+                time.sleep(1)
